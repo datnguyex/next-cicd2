4e8d91362483961aa00b0bddc4a48b378ec881b0
datnguyex <nnguyendat72@gmail.com>
2025-02-15 23:15:56 +0700

    update 12

--- __tests__/snapshot.tsx
+++ __tests__/snapshot.tsx
@@ -5,6 +5,6 @@ import { render } from "@testing-library/react";
 import Home from "@/pages/home/index";
 
 it("renders homepage unchanged", () => {
-  // // const { container } = render(<Home />);
-  // expect(container).toMatchSnapshot();
+  const { container } = render(<Home />);
+  expect(container).toMatchSnapshot();
 });
